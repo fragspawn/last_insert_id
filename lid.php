18e0ec12ab792da6a10b3a249c680ada4e4b7d49
developer <developer@webserver.tafeqld.dd>
2017-08-28 10:30:41 +1000

    first commit

---
+++ lid.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * lastInsertId is a PDO method that is used to programatically 
+ * enforce referential integrity. 
+ *
+ * How would you chain two insert actions ensuring primary &
+ * secondary keys have integrity.
+ *
+ * Any database example from your assignment will do.
+ *
+ */
+
+
+
+?>
